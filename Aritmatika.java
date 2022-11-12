3efadc09666e9db3ca0bbbdff74633bd6c8f1cd1
julia-kheil <115854674+julia-kheil@users.noreply.github.com>
2022-11-12 11:40:14 +0700

    Add files via upload

---
+++ Aritmatika.java
@@ -0,0 +1,35 @@
+package ekspresi;
+
+public class Aritmatika {
+	public static void main(String[] args) {
+		int x = 4;
+		int y = 2;
+		int z = 3;
+		System.out.println("nilai x = " + x) ;
+		System.out.println("nilai x = " + y) ;
+		System.out.print("x + y = ") ;
+		System.out.println(x + y);
+		System.out.print("x - y = ") ;
+		System.out.println(x - y);
+		System.out.print("x * y = ") ;
+		System.out.println(x * y);
+		System.out.print("x / y = ") ;
+		System.out.println(x / y) ;
+		System.out.print("x mod y = ") ;
+		System.out.println(x % y) ;
+		System.out.print("x mod z = ") ;
+		System.out.println(x % z);
+		
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ Concat.java
@@ -0,0 +1,6 @@
+package ekspresi;
+public class Concat{
+	public static void main (String[] args) {
+		System.out.println("hello " + "word");
+	}
+}
